30fd6e77f4b981b15b836853c6b97a6f017b1b56
kosuna95 <66427040+kosuna95@users.noreply.github.com>
2025-05-11 22:09:05 -0300

    Add AndroidKeyboardApp/app/src/test/java/com/trg/customkeyboard/ExampleUnitTest.java 1747012146220

---
+++ AndroidKeyboardApp/app/src/test/java/com/trg/customkeyboard/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.trg.customkeyboard;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
